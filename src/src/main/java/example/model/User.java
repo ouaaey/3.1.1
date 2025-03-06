dabfc864eb62d67bd523d2279728816680db9149
Irina <ouaaey@gmail.com>
2025-03-06 19:22:33 +0400

    3.1.1

--- src/src/main/java/example/model/User.java
+++ src/src/main/java/example/model/User.java
@@ -1,7 +1,15 @@
 package example.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users") //реализовала JBV_API
